200b5a3d023418438707937450b5846ef8074d6b
luca-rizzo <86370574+luca-rizzo@users.noreply.github.com>
2021-06-29 16:57:51 +0200

    Aggiunta removeFile e fix bug

--- src/api/api.c
+++ src/api/api.c
@@ -1,12 +1,14 @@
-#include<api.h>
 #include<util.h>
+#include<api.h>
+#include<conn.h>
 int fd_skt=-1;
 char* socketname=NULL;
 int openConnection(const char* sockname, int msec, const struct timespec abstime){
     struct sockaddr_un sa;
-    NULLSYSCALL(sockname,malloc(UNIX_PATH_MAX),"malloc");
-    strncpy(socketname, sockname, UNIX_PATH_MAX);
-    strncpy(sa.sun_path, socketname, UNIX_PATH_MAX);
+    int len=strlen(sockname);
+    NULLSYSCALL(socketname, malloc(len+1),"malloc");
+    strncpy(socketname, sockname, len+1);
+    strncpy(sa.sun_path, socketname, len+1);
     sa.sun_family = AF_UNIX;
     NEGSYSCALL(fd_skt,socket(AF_UNIX, SOCK_STREAM,0),"socket");
     while(connect(fd_skt,(struct sockaddr *) &sa, sizeof(sa)) == -1){
@@ -22,7 +24,9 @@ int openConnection(const char* sockname, int msec, const struct timespec abstime
     return 0;
 }
 int closeConnection(const char* sockname){
-    if(strncmp(sockname,socketname,UNIX_PATH_MAX)!=0){
+    int len=strlen(sockname);
+    if(strncmp(sockname,socketname,len+1)!=0){
+        errno=ENOTCONN;
         return -1;
     }
     int notused;
@@ -32,34 +36,35 @@ int closeConnection(const char* sockname){
     fd_skt=-1;
     return 0;
 }
-int OpenFile(const char* pathname,int flags){
+int openFile(const char* pathname,int flags){
     if(pathname==NULL){
-        errno =ENOENT;
+        errno =EINVAL;
         return -1;
     }
     if(fd_skt==-1){
         errno=ENOTCONN;
         return -1;
     }
-    if(writen(fd,"op",2)==-1){//secondo il protocollo di comunicazione invio operazione da effettuare
+    t_op operazione=op; //open
+    if(writen(fd_skt,&operazione,sizeof(t_op))==-1){//secondo il protocollo di comunicazione invio operazione da effettuare
         return -1; //errno settata da writen
     }
-    int len=strlen(pathname);
-    if(writen(fd,&len,sizeof(int))==-1){//secondo il protocollo di comunicazione invio lunghezza path, path del file da aprire e relativi flag
+    int len=strlen(pathname)+1;
+    if(writen(fd_skt,&len,sizeof(int))==-1){//secondo il protocollo di comunicazione invio lunghezza path, path del file da aprire e relativi flag
         return -1;
     }
-    if(writen(fd,pathname,len)==-1){
+    if(writen(fd_skt,(void*)pathname,len)==-1){
         return -1;
     }
-    if(writen(fd,&flags,sizeof(int))==-1){
+    if(writen(fd_skt,&flags,sizeof(int))==-1){
         return -1;
     }
     int res;
-    if(readn(fd,&res,sizeof(int))==-1){ //leggo il risultato dell'operazione
+    if(readn(fd_skt,&res,sizeof(int))==-1){ //leggo il risultato dell'operazione
         return -1;
     }
-    if(r!=0){
-        errno=r;
+    if(res!=0){
+        errno=res;
         return -1;
     } 
     //l'operazione è terminata correttamente -> il file è stato aperto
@@ -67,47 +72,52 @@ int OpenFile(const char* pathname,int flags){
 }
 int readFile(const char* pathname, void** buf, size_t* size){
     if(pathname==NULL){
-        errno =ENOENT;
+        errno =EINVAL;
         return -1;
     }
     if(fd_skt==-1){
         errno=ENOTCONN;
         return -1;
     }
-    if(writen(fd,"rd",2)==-1){//secondo il protocollo di comunicazione invio operazione da effettuare
+    t_op operazione=rd;
+    if(writen(fd_skt,&operazione,sizeof(t_op))==-1){//secondo il protocollo di comunicazione invio operazione da effettuare
         return -1; //errno settata da writen
     }
-    int len=strlen(pathname);
-    if(writen(fd,&len,sizeof(int))==-1){//secondo il protocollo di comunicazione invio lunghezza path e path del file da leggere
+    int len=strlen(pathname)+1;
+    if(writen(fd_skt,&len,sizeof(int))==-1){//secondo il protocollo di comunicazione invio lunghezza path e path del file da leggere
         return -1;
     }
-    if(writen(fd,pathname,len)==-1){
+    if(writen(fd_skt,(void*)pathname,len)==-1){
         return -1;
     }
     int res;
-    if(readn(fd,&res,sizeof(int))==-1){ //leggo il risultato dell'operazione
+    if(readn(fd_skt,&res,sizeof(int))==-1){ //leggo il risultato dell'operazione
         return -1;
     }
-    if(r!=0){
-        errno=r;
+    if(res!=0){
+        errno=res;
         return -1;
     } 
     //l'operazione è terminata correttamente
-    if(readn(fd,&len,sizeof(int))==-1){ //leggo la dimensione del file
+    size_t dim;
+    if(readn(fd_skt,&dim,sizeof(size_t))==-1){ //leggo la dimensione del file
         return -1;
     }
-    char* buffer=malloc(sizeof(len)); //alloco un buffer di tale dimensione
-    if(readn(fd,buffer,len)==-1){ //leggo contenuto file
-        return -1;
+    void* buffer=NULL;
+    if(dim!=0){
+        buffer=malloc(dim); //alloco un buffer di tale dimensione
+        if(readn(fd_skt,buffer,dim)==-1){ //leggo contenuto file
+            return -1;
+        }
     }
-    *size=len; //ritorno byte letti
+    *size=dim; //ritorno byte letti
     (*buf)=buffer; //ritorno contenuto file
     return 0;
 }
 //permette di scrivere sul server un file solo se l'ultima operazione sul file è stata la sua creazione
-int WriteFile(const char* pathname, const chear* dirname){
+int writeFile(const char* pathname, const char* dirname){
     if(pathname==NULL){
-        errno =ENOENT;
+        errno =EINVAL;
         return -1;
     }
     if(fd_skt==-1){
@@ -115,7 +125,7 @@ int WriteFile(const char* pathname, const chear* dirname){
         return -1;
     }
     FILE* ifp;
-    if((ifp=fopen(pathname),"rb")==-1){ //apro il file
+    if((ifp=fopen(pathname,"rb"))==NULL){ //apro il file
         return -1; //setta errno
     }
     //scopro la dimensione del file
@@ -125,15 +135,15 @@ int WriteFile(const char* pathname, const chear* dirname){
         }
         return -1;
     }
-    long size = ftell(ifp);
+    size_t size = ftell(ifp);
     if(size==-1){
         if(fclose(ifp)!=0){
             return -1;
         }
-        return -1
+        return -1;
     }
     rewind(ifp);
-    void* buffer= malloc(sizeof(size));
+    void* buffer= malloc(size);
     if(fread(buffer,1,size,ifp)<size){
         free(buffer);
         errno = EIO;
@@ -143,7 +153,8 @@ int WriteFile(const char* pathname, const chear* dirname){
         return -1;
     }
     //abbiamo letto il contenuto del file in buffer; adesso lo mandiamo al server
-    if(writen(fd,"wr",2)==-1){//secondo il protocollo di comunicazione invio operazione da effettuare
+    t_op operazione=wr;
+    if(writen(fd_skt,&operazione,sizeof(t_op))==-1){//secondo il protocollo di comunicazione invio operazione da effettuare
         free(buffer);
         if(fclose(ifp)!=0){
             return -1;
@@ -151,15 +162,15 @@ int WriteFile(const char* pathname, const chear* dirname){
         return -1; //errno settata da writen
     }
     //secondo il protocollo di comunicazione invio lunghezza path e path del file da scrivere
-    int len=strlen(pathname);
-    if(writen(fd,&len,sizeof(int))==-1){
+    int len=strlen(pathname)+1;
+    if(writen(fd_skt,&len,sizeof(int))==-1){
         free(buffer);
         if(fclose(ifp)!=0){
             return -1;
         }
         return -1;
     }
-    if(writen(fd,pathname,len)==-1){
+    if(writen(fd_skt,(void*)pathname,len)==-1){
         free(buffer);
         if(fclose(ifp)!=0){
             return -1;
@@ -167,14 +178,14 @@ int WriteFile(const char* pathname, const chear* dirname){
         return -1;
     }
     //secondo il protocollo di comunicazione invio dimensione file e contenuto
-    if(writen(fd,&size,sizeof(int))==-1){
+    if(writen(fd_skt,&size,sizeof(size_t))==-1){
         free(buffer);
         if(fclose(ifp)!=0){
             return -1;
         }
         return -1;
     }
-    if(writen(fd, buffer, size)==-1){
+    if(writen(fd_skt, buffer, size)==-1){
         free(buffer);
         if(fclose(ifp)!=0){
             return -1;
@@ -182,19 +193,19 @@ int WriteFile(const char* pathname, const chear* dirname){
         return -1;
     }
     int res;
-    if(readn(fd,&res,sizeof(int))==-1){ //leggo il risultato dell'operazione
+    if(readn(fd_skt,&res,sizeof(int))==-1){ //leggo il risultato dell'operazione
         free(buffer);
         if(fclose(ifp)!=0){
             return -1;
         }
         return -1;
     }
-    if(r!=0){
+    if(res!=0){
         free(buffer);
         if(fclose(ifp)!=0){
             return -1;
         }
-        errno=r;
+        errno=res;
         return -1;
     }
     free(buffer);
@@ -202,5 +213,36 @@ int WriteFile(const char* pathname, const chear* dirname){
         return -1;
     }
     //operazione terminata correttamente
+    return 0;
+}
+int removeFile(const char* pathname){
+    if(pathname==NULL){
+        errno = EINVAL;
+        return -1;
+    }
+    if(fd_skt==-1){
+        errno=ENOTCONN;
+        return -1;
+    }
+    t_op operazione=re;
+    if(writen(fd_skt,&operazione,sizeof(t_op))==-1){//secondo il protocollo di comunicazione invio operazione da effettuare
+        return -1; //errno settata da writen
+    }
+    int len=strlen(pathname)+1;
+    if(writen(fd_skt,&len,sizeof(int))==-1){//secondo il protocollo di comunicazione invio lunghezza path e path del file da leggere
+        return -1;
+    }
+    if(writen(fd_skt,(void*)pathname,len)==-1){
+        return -1;
+    }
+    int res;
+    if(readn(fd_skt,&res,sizeof(int))==-1){ //leggo il risultato dell'operazione
+        return -1;
+    }
+    if(res!=0){
+        errno=res;
+        return -1;
+    } 
+    //l'operazione è terminata correttamente -> il file è stato cancellato
     return 0;
 }
