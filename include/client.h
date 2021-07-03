e942ee91144336112cff80ba70da2a4960b9868e
luca-rizzo <86370574+luca-rizzo@users.noreply.github.com>
2021-07-03 16:28:13 +0200

    Create client.h

---
+++ include/client.h
@@ -0,0 +1,30 @@
+#if !defined(CLIENT_H)
+#define CLIENT_H
+#define MAX_PATH_LENGTH 255
+#define IFSTOUT(s) if(config.stampaStOut) (s);
+typedef enum{
+    WriteFile,
+    ReadFile,
+    WhereToSave,
+    ReadNFiles,
+    DeleteFile
+}tipoOperazione;
+
+typedef struct{
+    tipoOperazione op;
+    char* path;
+    int n;
+    char* pathToSave;
+}richiesta;
+
+typedef struct{
+    int stampaStOut; //opzione che mi dice se stampare sullo standard output le varie richieste
+    long millisec; //opzione (settata con -t) che mi dice il tempo che deve intercorrere tra l'invio di due richieste 
+    char* socket; //indirizzo socket a cui connettersi
+}config_client;
+
+int ottieniRichieste(config_client* config, t_coda* richieste, int argc, char** argv);
+int validazioneRichieste(t_coda* richieste);
+int eseguiRichieste(t_coda* richieste, config_client config);
+void freeRichiesta(void* val);
+#endif
