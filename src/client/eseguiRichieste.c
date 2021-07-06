882415d05d1920cbfe25b8d73b85a37e652887e2
luca-rizzo <86370574+luca-rizzo@users.noreply.github.com>
2021-07-06 15:58:21 +0200

    migliorate stampe

--- src/client/eseguiRichieste.c
+++ src/client/eseguiRichieste.c
@@ -1,65 +1,130 @@
-#include<util.h>
-#include<coda.h>
 #include<client.h>
-#include<api.h>
 int eseguiRichieste(t_coda* richieste, config_client config){
+    int err;
     void* buffer;
     size_t size;
     nodo* p;
+    struct timespec time;
+    time.tv_sec = config.millisec / 1000;
+    time.tv_nsec = (config.millisec % 1000) * 1000000;
     p=prelevaDaCoda(richieste);
     while(p!=NULL){
         richiesta* r=(richiesta*)p->val;
         switch(r->op){
             case WriteFile:
                 if(openFile(r->path,O_CREAT)==-1){
-                    perror("openFile");
-                    IFSTOUT(fprintf(stdout,"Tipo Operazione: Scrittura file \nFile di riferimento: %s\nEsito: Negativo\n\n\n", r->path))
+                    if(config.stampaStOut){
+                        fprintf(stderr,"Tipo Operazione: Scrittura file \nFile di riferimento: %s\nEsito: Negativo\nMotivazione: ", r->path);
+                        perror("API--openFile");
+                    }
                     break;
                 }
-                if(writeFile(r->path,NULL)==-1){
-                    perror("writeFile");
-                    IFSTOUT(fprintf(stdout,"Tipo Operazione: Scrittura file \nFile di riferimento: %s\nEsito: Negativo\n\n\n", r->path))
-                }
+                if((err=writeFile(r->path,NULL))==-1){
+                    if(config.stampaStOut){
+                        fprintf(stderr,"Tipo Operazione: Scrittura file \nFile di riferimento: %s\nEsito: Negativo\nMotivazione: ", r->path);
+                        perror("API--writeFile");
+                    }
+                }//devo comunque chiudere il file
                 if(closeFile(r->path)==-1){
-                    perror("closeFile");
+                    if(config.stampaStOut){
+                        perror("API--closeFile");
+                    }
                     break;
                 }
-                IFSTOUT(fprintf(stdout,"Tipo Operazione: Scrittura file \nFile di riferimento: %s\nEsito: Positivo\n\n\n", r->path))
+                if(err!=-1 && config.stampaStOut)
+                    fprintf(stdout,"Tipo Operazione: Scrittura file \nFile di riferimento: %s\nEsito: Positivo\n\n", r->path);
                 break;
             case ReadFile:
                 if(openFile(r->path,-1)==-1){
-                    perror("openFile");
-                    IFSTOUT(printf("Tipo Operazione: Lettura file \nFile di riferimento: %s\nEsito: Negativo\n\n\n", r->path))
+                    if(config.stampaStOut){
+                        fprintf(stderr,"Tipo Operazione: Lettura file \nFile di riferimento: %s\nEsito: Negativo\nMotivazione: ", r->path);
+                        perror("API--openFile");
+                    }
                     break;
                 }
-                if(readFile(r->path,&buffer,&size)==-1){
-                    perror("readFile");
-                    IFSTOUT(fprintf(stdout,"Tipo Operazione: Lettura file \nFile di riferimento: %s\nEsito: Negativo\n\n\n", r->path))
-                }
+                if((err=readFile(r->path,&buffer,&size))==-1){
+                    if(config.stampaStOut){
+                        fprintf(stderr,"Tipo Operazione: Lettura file \nFile di riferimento: %s\nEsito: Negativo\nMotivazione: ", r->path);
+                        perror("API--readFile");
+                    }
+                }//devo comunque chiudere il file
                 if(closeFile(r->path)==-1){
-                    perror("CloseFile");
+                    if(config.stampaStOut){
+                        perror("API--closeFile");
+                    }
                     break;
                 }
-                IFSTOUT(fprintf(stdout,"Tipo Operazione: Lettura file \nFile di riferimento: %s\nEsito: Positivo\nBytes letti: %ld\n Contenuto:\n%s\n\n", r->path,size,(char*)buffer))
+                if(err!=-1){
+                    if(r->pathToSave==NULL){
+                        if(config.stampaStOut)
+                            fprintf(stdout,"Tipo Operazione: Lettura file \nFile di riferimento: %s\nEsito: Positivo\nFile non salvato su disco\n\n", r->path);
+                    }
+                    else{
+                        if(scriviContenutoInDirectory(buffer,size,r->path,r->pathToSave)==-1){
+                            fprintf(stderr,"Errore nella scrittura su disco del file %s\n", (char*) r->path);
+                            perror("scriviContenutoInDirectory");
+                            if(config.stampaStOut)
+                                fprintf(stdout,"Tipo Operazione: Lettura file \nFile di riferimento: %s\nEsito: Positivo\nFile non salvato su disco\n\n", r->path);
+                        }  
+                        else{
+                            if(config.stampaStOut)
+                            fprintf(stdout,"Tipo Operazione: Lettura file \nFile di riferimento: %s\nEsito: Positivo\nFile salvati su disco nella directory: %s\n\n", r->path, r->pathToSave);
+                        }
+                    }
+                    if(buffer!=NULL)
+                        free(buffer);
+                }
                 break;
             case DeleteFile:
                 if(openFile(r->path,-1)==-1){
-                    perror("openFile");
-                    IFSTOUT(fprintf(stdout,"Tipo Operazione: Rimozione file \nFile di riferimento: %s\nEsito: Negativo\n\n\n", r->path))
+                    if(config.stampaStOut){
+                        fprintf(stderr,"Tipo Operazione: Rimozione file \nFile di riferimento: %s\nEsito: Negativo\nMotivazione: ", r->path);
+                        perror("API--openFile");
+                    }
                     break;
                 }
                 if(removeFile(r->path)==-1){
-                    perror("writeFile");
-                    IFSTOUT(fprintf(stdout,"Tipo Operazione: Rimozione file \nFile di riferimento: %s\nEsito: Negativo\n\n\n", r->path))
+                    if(config.stampaStOut){
+                        fprintf(stderr,"Tipo Operazione: Rimozione file \nFile di riferimento: %s\nEsito: Negativo\nMotivazione: ", r->path);
+                        perror("API--removeFile");
+                    }
+                    //devo comunque chiudere il file perchè non lo ho eliminato correttamente
+                    if(closeFile(r->path)==-1){
+                        if(config.stampaStOut){
+                            perror("API--closeFile");
+                        }
+                    }
+                    break;
                 }
-                IFSTOUT(fprintf(stdout,"Tipo Operazione: Rimozione file \nFile di riferimento: %s\nEsito: Positivo\n\n\n", r->path))
+                if(config.stampaStOut)
+                    fprintf(stdout,"Tipo Operazione: Rimozione file \nFile di riferimento: %s\nEsito: Positivo\n\n", r->path);
                 break;
-            case ReadNFile: 
+            case ReadNFiles:{
+                int nread;
+                if((nread=readNFiles(r->n,r->pathToSave))==-1){
+                    if(config.stampaStOut){
+                        fprintf(stderr,"Tipo Operazione: Lettura N file \nEsito: Negativo\nMotivazione: ");
+                        perror("API--readNFiles");
+                    }
+                }
+                else{
+                    if((r->pathToSave)!=NULL){
+                        if(config.stampaStOut)
+                            fprintf(stdout,"Tipo Operazione: Lettura N file \nEsito: Positivo\nFile letti: %d\nSalvati nella directory:%s\n\n", nread,r->pathToSave);
+                    }
+                    else{
+                        if(config.stampaStOut)
+                            fprintf(stdout,"Tipo Operazione: Lettura N file \nEsito: Positivo\nFile letti: %d\nFile non salvati su disco\n", nread);
+                    }
+                }
                 break;
+            }
             default:
                 break;
         }
-        sleep(config.millisec);
+        nanosleep(&time,NULL);
+        freeRichiesta(p->val);
+        free(p);
         p=prelevaDaCoda(richieste);
     }
     return 0;
